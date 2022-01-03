cd5ce965b768f07e8449bc4a86c3f3a608f88e41
situmbolon <97037628+situmbolon@users.noreply.github.com>
2022-01-03 21:40:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* Required settings     */
-$CLOAKING['WHITE_PAGE'] = 'aa.php';//PHP/HTML file or URL used for bots
+$CLOAKING['WHITE_PAGE'] = 'a.php';//PHP/HTML file or URL used for bots
 $CLOAKING['OFFER_PAGE'] = 'b.php';//PHP/HTML file or URL offer used for real users
 $CLOAKING['DEBUG_MODE'] = 'off';// replace "on" with "off" to switch from debug to production mode
 $CLOAKING['STEALTH'] = 'off';// replace "on" with "off" to hide stat page
